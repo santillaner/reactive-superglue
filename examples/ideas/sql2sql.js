aaa5061096db399b4b56bf86c1fa1026a621e3ad
Manuel Santillan <manuel.santillan@gmail.com>
2016-04-03 19:19:54 +0200

    changez

--- examples/ideas/sql2sql.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by santillan on 30/3/16.
- */
--- examples/sql.js
+++
@@ -1,8 +0,0 @@
-var _ = require("../")
-
-var db = _.sql("mysql://localhost/test")
-db
-    .select("select * from test_table limit 10")
-    .json()
-    .stdout()
-    .done(()=>db.end())
