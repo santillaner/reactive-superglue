90bbabe07545d14d49150564b38d8988abd9fedc
Manuel Santillan <manuel.santillan@gmail.com>
2016-04-03 19:06:20 +0200

    changez

--- examples/sql.js
+++ examples/sql.js
@@ -2,7 +2,7 @@ var _ = require("../")
 
 var db = _.sql("mysql://localhost/test")
 db
-    .select("select * from app_usage limit 10")
+    .select("select * from test_table limit 10")
     .json()
     .stdout()
     .done(()=>db.end())
